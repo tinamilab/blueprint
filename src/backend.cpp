54a92ad5c637c851e324e34daa60f7bb61115aec
cgonzalezbrito <cgonzalezbrt@gmail.com>
2019-12-15 23:14:10 -0500

    Backend code review

--- src/backend.cpp
+++ src/backend.cpp
@@ -19,6 +19,7 @@
 #define BLUEPRINT_USB_HID_PACKET_SIZE (BLUEPRINT_USB_DATA_PACKET_SIZE + 1)
 
 /*!
+ * CONSTRUCTOR
  * \brief BackEnd::BackEnd
  * \param parent
  */
@@ -69,8 +70,10 @@ BackEnd::~BackEnd()
  * TIMER OVERFLOW MAIN LOOP
  * \brief BackEnd::timer_timeout
  */
-void BackEnd::timer_timeout(){
-    switch (m_deviceStatus) {
+void BackEnd::timer_timeout()
+{
+    switch (m_deviceStatus)
+    {
     case Unplugged:
         senseDeviceStatus();
         break;
@@ -101,8 +104,10 @@ void BackEnd::timer_timeout(){
  * TIMER TO READ OVERFLOW
  * \brief BackEnd::timerRead_timeout
  */
-void BackEnd::timerRead_timeout(){
-    switch (preset_status) {
+void BackEnd::timerRead_timeout()
+{
+    switch (preset_status)
+    {
     case Request_Preset:
         redPresetReq(m_preset);
         break;
@@ -116,8 +121,10 @@ void BackEnd::timerRead_timeout(){
  * TIMER SYNC OVERFLOW
  * \brief BackEnd::timerSync_timeout
  */
-void BackEnd::timerSync_timeout(){
-    switch (sync_status) {
+void BackEnd::timerSync_timeout()
+{
+    switch (sync_status)
+    {
     case Request_Sync:
         syncReq();
         break;
@@ -140,10 +147,11 @@ void BackEnd::timerSync_timeout(){
 }
 
 /*!
+ * DEVICE STATUS
  * \brief BackEnd::senseDeviceStatus
  */
-void BackEnd::senseDeviceStatus(){
-
+void BackEnd::senseDeviceStatus()
+{
     this->timerLoop->stop();
     setDeviceStatus(Unplugged);
 
@@ -165,8 +173,8 @@ void BackEnd::senseDeviceStatus(){
     return;
 }
 
-void BackEnd::setDeviceStatus(const DeviceStatus &deviceStatus){
-
+void BackEnd::setDeviceStatus(const DeviceStatus &deviceStatus)
+{
     if (deviceStatus == m_deviceStatus)
         return;
 
@@ -202,28 +210,24 @@ BackEnd::ErrorCode BackEnd::open(uint16_t deviceVIDtoOpen, uint16_t devicePIDtoO
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///                                       DEVICE CONFIGURATION                                                          //
 /// \brief BackEnd::senseDeviceStatus                                                                                   //
-///
-///  HOST REQUEST READING
-//
-//   ___________________________ ___________________________ ___________________________ ___________________________
-//  |           SOF             |   HOST_REQUIRES_READING   |           CMD             |   HOST_REQUIRES_READING   |
-//   --------------------------- --------------------------- --------------------------- ---------------------------
-//
-//  HOST REQUEST READING
-//
-//   ___________________________ ___________________________ ___________________________ ___________________________
-//  |           SOF             |   HOST_REQUIRES_READING   |           CMD             |   HOST_REQUIRES_READING   |
-//   --------------------------- --------------------------- --------------------------- ---------------------------
-//
-//  HOST REQUEST READING
-//
-//   ___________________________ ___________________________ ___________________________ ___________________________
-//  |           SOF             |   HOST_REQUIRES_READING   |           CMD             |   HOST_REQUIRES_READING   |
-//   --------------------------- --------------------------- --------------------------- ---------------------------                                                                                       //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                                     //
+///  HOST REQUEST READING                                                                                               //
+///                                                                                                                     //
+///   ___________________________ ___________________________ ___________________________ ___________________________   //
+///  |           SOF             |   HOST_REQUIRES_READING   |           CMD             |          PRESET           |  //
+///   --------------------------- --------------------------- --------------------------- ---------------------------   //
+///                                                                                                                     //
+///  FINISH (LAST BYTE)                                                                                                 //
+///                                                                                                                     //
+///   ___________________________ ___________________________ ___________________________ ___________________________   //
+///  |           SOF             |           FINISH          |           CMD             |          PRESET           |  //
+///   --------------------------- --------------------------- --------------------------- ---------------------------   //
+///                                                                                                                     //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void BackEnd::readDeviceConfiguration(ConfigStatus &conf_state){
-
+void BackEnd::readDeviceConfiguration(ConfigStatus &conf_state)
+{
     unsigned char data_out[BLUEPRINT_USB_HID_PACKET_SIZE]; //data to write
     unsigned char data_in[BLUEPRINT_USB_HID_PACKET_SIZE]; //data to read
 
@@ -231,9 +235,12 @@ void BackEnd::readDeviceConfiguration(ConfigStatus &conf_state){
 
     switch (conf_state) {
     case Request:
+
+        this->timerLoop->stop();
         memset(data_out,0,BLUEPRINT_USB_HID_PACKET_SIZE);
         qDebug() << "\nReading device configuration\n";
-        this->timerLoop->stop();
+
+        /*HOST REQUEST READING */
         data_out[0]=0; data_out[1]=SOF; data_out[2]=HOST_REQUIRES_READING; data_out[3]=CMD_LAYOUT; data_out[4]=0;
 
         hid_write(md1_device,data_out,BLUEPRINT_USB_HID_PACKET_SIZE);
@@ -245,17 +252,19 @@ void BackEnd::readDeviceConfiguration(ConfigStatus &conf_state){
             this->timerLoop->start(timerLoopInterval);
             return;
         }
+
         conf_state = WaitFinish;
         packet_num_buffer = 0;
         this->timerLoop->start(100);
         break;
 
     case WaitFinish:
+
         this->timerLoop->stop();
+        memset(data_in,0,BLUEPRINT_USB_HID_PACKET_SIZE);
         qDebug() << "Reading Data";
         int offset;
 
-        memset(data_in,0,BLUEPRINT_USB_HID_PACKET_SIZE);
         if (hid_read_timeout(md1_device, data_in, BLUEPRINT_USB_HID_PACKET_SIZE, 1000) < 0){
             qDebug() << " write error";
             setDeviceStatus(Unplugged);
@@ -287,13 +296,12 @@ void BackEnd::readDeviceConfiguration(ConfigStatus &conf_state){
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///                                     SET LAYOUT
-/// \brief BackEnd::setLayout
-///                                     ENHANCE
-///
+///                                               SET LAYOUT                                                            //
+/// \brief BackEnd::setLayout                                                                                           //
+///                                                                                                                     //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void BackEnd::setLayout(){
-
+void BackEnd::setLayout()
+{
     this->timerLoop->stop();
 
     qDebug() << "Setting Layout";
@@ -323,7 +331,8 @@ void BackEnd::SetControlType(QList<QVariant> &controlType)
     return;
 }
 
-void BackEnd::setSynchronizing(bool &sync){
+void BackEnd::setSynchronizing(bool &sync)
+{
     if (sync == m_sync)
         return;
     m_sync = sync;
@@ -331,30 +340,35 @@ void BackEnd::setSynchronizing(bool &sync){
     return;
 }
 
-///////////////////////////////////////////////////////////////
-///
-///             Attributes
-///
-///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                                     //
+///                                             SET ATRIBUTTES                                                          //
+///                                                                                                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*!
  * \brief BackEnd::setPreset
  * \param preset
  */
-void BackEnd::setPreset(const unsigned char &preset){
+void BackEnd::setPreset(const unsigned char &preset)
+{
     if (preset == m_preset or m_deviceStatus != Working)
         return;
     m_preset = preset;
     m_globalChannel = layout[BLUEPRINT_PRESET_GLOBAL_CHANNEL_INDEX + preset];
     bool already_read = false;
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < 16; ++i)
+    {
         if (m_preset == preset_array[i])
             already_read = true;
     }
-    if (!already_read){
+    if (!already_read)
+    {
         readPreset(m_preset);
-        for (int i = 1; i < 16; ++i) {
-            if (preset_array[i] == 0){
+        for (int i = 1; i < 16; ++i)
+        {
+            if (preset_array[i] == 0)
+            {
                 preset_array[i] = m_preset;
                 break;
             }
@@ -366,7 +380,29 @@ void BackEnd::setPreset(const unsigned char &preset){
     emit controlTypeChanged();
 }
 
-void BackEnd::setComponentMode(const ComponentMode &deviceMode){
+/*!
+ * \brief BackEnd::setGlobalChannel
+ * \param globalChannel
+ */
+
+void BackEnd::setGlobalChannel(const unsigned char &globalChannel)
+{
+    if(globalChannel == m_globalChannel or m_deviceStatus != Working)
+        return;
+    qDebug() << "Global channel:" << globalChannel << "Preset:" << m_preset;
+    m_globalChannel = globalChannel;
+    layout[BLUEPRINT_PRESET_GLOBAL_CHANNEL_INDEX + m_preset] = m_globalChannel;
+    emit globalChannelChanged();
+    return;
+}
+
+/*!
+ * \brief BackEnd::setComponentMode
+ * \param deviceMode
+ */
+
+void BackEnd::setComponentMode(const ComponentMode &deviceMode)
+{
     int index = BLUEPRINT_PRESET_DATA_SIZE * m_preset + BLUEPRINT_CONTROL_DATA_SIZE * m_component;
 
     qDebug() << index << m_preset << m_component << deviceMode;
@@ -382,11 +418,36 @@ void BackEnd::setComponentMode(const ComponentMode &deviceMode){
     return;
 }
 
-void BackEnd::setComponentChannel(const unsigned char &controlChannel){
+/*!
+ * CC NUMBER
+ * \brief BackEnd::setComponentChannel
+ * \param controlChannel
+ */
+
+void BackEnd::setComponentData(const unsigned char &controlData)
+{
+    if(controlData == m_componentData or m_deviceStatus != Working)
+        return;
+
+    configuration.preset[m_preset].component[m_component].bytes.data = controlData;
+    m_componentData = controlData;
+
+    emit componentDataChanged();
+    return;
+}
+
+/*!
+ * \brief BackEnd::setComponentChannel
+ * \param controlChannel
+ */
+
+void BackEnd::setComponentChannel(const unsigned char &controlChannel)
+{
 
     if(controlChannel == m_componentChannel or m_deviceStatus != Working)
         return;
-    if (controlChannel >= 16){
+    if (controlChannel >= 16)
+    {
         configuration.preset[m_preset].component[m_component].bytes.channel = 0xFF;
         m_componentChannel = 0xFF;
     } else {
@@ -398,26 +459,10 @@ void BackEnd::setComponentChannel(const unsigned char &controlChannel){
     return;
 }
 
-void BackEnd::setGlobalChannel(const unsigned char &globalChannel){
-    if(globalChannel == m_globalChannel or m_deviceStatus != Working)
-        return;
-    qDebug() << "Global channel:" << globalChannel << "Preset:" << m_preset;
-    m_globalChannel = globalChannel;
-    layout[BLUEPRINT_PRESET_GLOBAL_CHANNEL_INDEX + m_preset] = m_globalChannel;
-    emit globalChannelChanged();
-    return;
-}
-
-void BackEnd::setComponentData(const unsigned char &controlData){
-    if(controlData == m_componentData or m_deviceStatus != Working)
-        return;
-
-    configuration.preset[m_preset].component[m_component].bytes.data = controlData;
-    m_componentData = controlData;
-
-    emit componentDataChanged();
-    return;
-}
+/*!
+ * \brief BackEnd::setComponentMinValue
+ * \param minValue
+ */
 
 void BackEnd::setComponentMinValue(const unsigned char &minValue){
     if (minValue == m_componentMinValue or m_deviceStatus != Working)
@@ -430,7 +475,13 @@ void BackEnd::setComponentMinValue(const unsigned char &minValue){
     return;
 }
 
-void BackEnd::setComponentMaxValue(const unsigned char &maxValue){
+/*!
+ * \brief BackEnd::setComponentMaxValue
+ * \param maxValue
+ */
+
+void BackEnd::setComponentMaxValue(const unsigned char &maxValue)
+{
     if (maxValue == m_componentMaxValue or m_deviceStatus != Working)
         return;
 
@@ -441,7 +492,13 @@ void BackEnd::setComponentMaxValue(const unsigned char &maxValue){
     return;
 }
 
-void BackEnd::setComponentButtonBehaviour(const ComponentButtonBehaviour &controlButtonBehaviour){
+/*!
+ * \brief BackEnd::setComponentButtonBehaviour
+ * \param controlButtonBehaviour
+ */
+
+void BackEnd::setComponentButtonBehaviour(const ComponentButtonBehaviour &controlButtonBehaviour)
+{
     if (controlButtonBehaviour == m_componentButtonBehaviour or m_deviceStatus != Working)
         return;
 
@@ -452,49 +509,61 @@ void BackEnd::setComponentButtonBehaviour(const ComponentButtonBehaviour &contro
     return;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                                     //
+///                                             SELECT COMPONENT                                                        //
+///                                                                                                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 /*!
  * \brief BackEnd::selectComponent
  * \param component
  */
-void BackEnd::selectComponent(const unsigned char &component){
-    unsigned char config;
+
+void BackEnd::selectComponent(const unsigned char &component)
+{
     unsigned char mode;
-    unsigned char channel;
     unsigned char data;
+    unsigned char channel;
     unsigned char minValue;
     unsigned char maxValue;
-
+    unsigned char config; /* Button Behaviour*/
     m_component = component;
 
     qDebug()<< "Selecting component" << component;
 
-    config = configuration.preset[m_preset].component[m_component].bytes.config;
     mode = configuration.preset[m_preset].component[m_component].bytes.mode;
-    channel = configuration.preset[m_preset].component[m_component].bytes.channel;
     data = configuration.preset[m_preset].component[m_component].bytes.data;
+    channel = configuration.preset[m_preset].component[m_component].bytes.channel;
     minValue = configuration.preset[m_preset].component[m_component].bytes.min;
     maxValue = configuration.preset[m_preset].component[m_component].bytes.max;
+    config = configuration.preset[m_preset].component[m_component].bytes.config;
 
-    setComponentButtonBehaviour(static_cast<ComponentButtonBehaviour>(config));
     setComponentMode(static_cast<ComponentMode>(mode));
-    setComponentChannel(channel);
     setComponentData(data);
+    setComponentChannel(channel);
     setComponentMinValue(minValue);
     setComponentMaxValue(maxValue);
-
+    setComponentButtonBehaviour(static_cast<ComponentButtonBehaviour>(config));
 
     emit componentChanged();
 
     return;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                                                     //
+///                                             SELECT PRESET                                                           //
+///                                                                                                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /*!
- * SELECT  & READ PRESET
  * \brief BackEnd::readPreset
  * \param preset
  */
-void BackEnd::readPreset(const unsigned char &preset){
-
+void BackEnd::readPreset(const unsigned char &preset)
+{
     this->timerLoop->stop();
     m_preset = preset;
     packet_num_buffer = 0;
@@ -503,8 +572,8 @@ void BackEnd::readPreset(const unsigned char &preset){
 
 }
 
-void BackEnd::redPresetReq(const unsigned char &preset){
-
+void BackEnd::redPresetReq(const unsigned char &preset)
+{
     this ->timerRead->stop();
 
     unsigned char data_out[BLUEPRINT_USB_HID_PACKET_SIZE]; //data to write
@@ -679,26 +748,6 @@ void BackEnd::SendReqPresetSync(){
     sync_status = SendPreset_Sync;
     return;
 }
-
-//void BackEnd::WaitOK1Sync(){
-
-//    unsigned char data_in[BLUEPRINT_USB_HID_PACKET_SIZE]; //data to read
-//    memset(data_in,0,BLUEPRINT_USB_HID_PACKET_SIZE);
-
-//    if (hid_read_timeout(md1_device, data_in, BLUEPRINT_USB_HID_PACKET_SIZE, 1000) < 0){
-//        setDeviceStatus(Unplugged);
-//        qDebug() << " read error";
-//        this->timerSync->stop();
-//        this->timerLoop->start(timerLoopInterval);
-//        return;
-//    }
-
-//    if((data_in[0] == SOF) && (data_in[1] == OK) ){
-//        qDebug() << "Ok";
-//        sync_status = SendPreset_Sync;
-//    }
-//    return;
-//}
 
 void BackEnd::SendPresetSync(){
 
